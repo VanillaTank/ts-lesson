186fd794edaf40c950618b89232459282d3356c8
Tatiana Mikhailova <tedien2016@yandex.ru>
2021-10-21 15:18:09 +0800

    part-10 done

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,8 +1,20 @@
 //создаем класс-обертку для того, чтобы ограничить доступ к некоторым методам в целях безопасности
+
+
+//В TS классы имеют двойную природу: они могут быть использованы для создания экземпляров этого класса и для ссылки на такой тип, например { User } - образец и тип. Здесь это не используется, так как юзаем interface Mapable 
+
 import { User } from "User";
 import { Company } from "Company";
 
-//В TS классы имеют двойную природу: они могут быть использованы для создания экземпляров этого класса и для ссылки на такой тип, например { User } - образец и тип 
+//инструкия, как быть агрументом для addMarker()
+export interface Mapable {
+  location: {
+    lat: number;
+    lng: number;
+  }
+  markerContent():string;
+  color: string;
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -17,13 +29,20 @@ export class CustomMap {
   // можем передавать в метод или объект класса User или объект класса Company
   // TS позволит использовать только общие свойства, в данном случае - location
   // но такой подход не масштабируется (например, если нам нужно передать 50 вариантов mapable )
-  addMarker(mapable: User | Company): void {
-    new google.maps.Marker({
+  addMarker(mapable: Mapable): void {
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mapable.location.lat,
         lng: mapable.location.lng
       }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content : 'Привет, мир!'
+      })
+      infoWindow.open(this.googleMap, marker);
     })
   }
 
--- src/User.ts
+++ src/User.ts
@@ -1,18 +1,21 @@
 import faker from 'faker';
-
+import { Mapable } from 'CustomMap';
 
 //пример экспорта  (см. импорт в index.ts)
 //стараемся не использовать в своем коде export default, тк принято писать импорт через {}
 export const name : string = 'Jack' 
 export default 'thing for import';
 
-
-export class User {
+//все элементы класса User должны соответствовать Mapable
+// говорит implements Mapable
+export class User implements Mapable{
     name : string;
     location : {
         lat : number;
         lng : number;
     };
+
+    color: string = 'black';
 
     constructor () {
         this.name = faker.name.firstName();
@@ -20,5 +23,9 @@ export class User {
             lat : parseFloat(faker.address.latitude()),
             lng : parseFloat(faker.address.longitude())
         }
+    }
+
+    markerContent():string {
+        return `User Name: ${this.name}`
     }
 }
